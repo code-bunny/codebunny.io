2ebe8f02c3aabb8fe22d6ec9556e0125517f0b99
David Roy <david.roy@codebunnies.co.uk>
2023-10-04 19:17:27 +0100

    removes some unneeded content

--- app/page.tsx
+++ app/page.tsx
@@ -33,30 +33,6 @@ export default async function Home() {
               <section>
                 <h2 className="font-aspekta text-xl font-[650] mb-3">Latest Articles</h2>
 
-                {/* Filters */}
-                <ul className="flex flex-wrap text-sm border-b border-slate-100 dark:border-slate-800">
-                  <li className="px-3 -mb-px">
-                    <a className="block py-3 font-medium text-slate-800 dark:text-slate-100 border-b-2 border-sky-500" href="#0">
-                      Coding
-                    </a>
-                  </li>
-                  <li className="px-3 -mb-px">
-                    <a className="block py-3 text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300" href="#0">
-                      Startups
-                    </a>
-                  </li>
-                  <li className="px-3 -mb-px">
-                    <a className="block py-3 text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300" href="#0">
-                      Tutorials
-                    </a>
-                  </li>
-                  <li className="px-3 -mb-px">
-                    <a className="block py-3 text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300" href="#0">
-                      Indie Hacking
-                    </a>
-                  </li>
-                </ul>
-
                 {/* Articles list */}
                 <div>
                   {allPosts.map((post, postIndex) => (
@@ -64,10 +40,6 @@ export default async function Home() {
                   ))}
                 </div>
               </section>
-
-              <Talks />
-              <FeaturedProjects />
-
             </div>
           </div>
         </div>
@@ -75,10 +47,6 @@ export default async function Home() {
         { /* Right sidebar */}
         <aside className="md:w-[240px] lg:w-[300px] shrink-0">
           <div className="space-y-6">
-
-            <WidgetNewsletter />
-            <WidgetSponsor />
-            <WidgetBook />
 
           </div>
         </aside>
